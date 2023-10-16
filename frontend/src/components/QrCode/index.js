2ad635e55c701047507d45dbb2d844b8067b731c
Pavel Drbalek <services@vakopako.ooo>
2023-10-16 21:08:32 +0200

    feat: implement base qr code [frontend]

---
+++ frontend/src/components/QrCode/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import QrCodeLibrary from 'qrcode';
+
+const HARDCODED_MESSAGE_GROUP_ID = 'c0b2b020-6787-11ee-8c99-0242ac120002';
+
+const errorCallback = (error) => {
+  if (error) console.error(error)
+}
+
+const QrCode = () => {
+  const canvasRef = React.useRef(null);
+
+  const qrCodeUrl = React.useMemo(() => {
+    return `https://www.message-box.local/${HARDCODED_MESSAGE_GROUP_ID}`;
+  }, []);
+
+  React.useEffect(() => {
+    QrCodeLibrary.toCanvas(canvasRef.current, qrCodeUrl, errorCallback)
+  }, [qrCodeUrl]);
+
+  return (
+    <canvas ref={canvasRef} />
+  );
+}
+
+export default QrCode;
