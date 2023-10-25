4e42f484a2052e2850ba375927c263c80b0659ca
Pavel Drbalek <services@vakopako.ooo>
2023-10-25 18:08:15 +0200

    refactor: get messages route [backend]

--- backend/src/routes/messages/getMany.ts
+++ backend/src/routes/messages/getMany.ts
@@ -3,22 +3,25 @@ import { Request, Response } from 'express';
 
 const prisma = new PrismaClient();
 
-const getManyMessages = (req: Request, res: Response) => {
+const getManyMessages = async (req: Request, res: Response) => {
   const { groupId } = req.params;
 
   if (!groupId) {
     return res.status(400).json({ error: 'Missing group ID' });
   }
 
-  const messages = prisma.message.findMany({
+  const messages = await prisma.message.findMany({
+    select: {
+      content: true,
+    },
     where: { groupId },
   });
 
   if (!messages) {
-    return res.status(404).json({ error: 'Messages not found' });
+    return res.status(500).json({ error: 'Failed to get messages' });
   }
 
-  return res.json({ data: messages });
+  res.json({ payload: messages });
 };
 
 export default getManyMessages;
