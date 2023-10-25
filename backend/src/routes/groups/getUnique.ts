d19c2246c9ee4eaaf65c2066d587380b9c758be9
Pavel Drbalek <services@vakopako.ooo>
2023-10-25 18:07:36 +0200

    refactor: get unique group route [backend]

--- backend/src/routes/groups/getUnique.ts
+++ backend/src/routes/groups/getUnique.ts
@@ -3,22 +3,22 @@ import { Request, Response } from 'express';
 
 const prisma = new PrismaClient();
 
-const getUniqueGroup = (req: Request, res: Response) => {
+const getUniqueGroup = async (req: Request, res: Response) => {
   const { groupId } = req.params;
 
   if (!groupId) {
     return res.status(400).json({ error: 'Missing group ID' });
   }
 
-  const group = prisma.group.findUnique({
+  const group = await prisma.group.findUnique({
     where: { id: groupId },
   });
 
   if (!group) {
-    return res.status(404).json({ error: 'Group not found' });
+    return res.status(500).json({ error: 'Failed to get group' });
   }
 
-  return res.json({ data: group });
+  res.json({ payload: group });
 };
 
 export default getUniqueGroup;
